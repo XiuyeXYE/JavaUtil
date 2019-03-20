cb54bfc35ca04913f3b6520e0be66c51b5adf1d3
xiuye <xiuye_engineer@outlook.com>
2019-03-20 16:43:24 +0800

    new functions

--- src/com/xiuye/util/log/LogUtil.java
+++ src/com/xiuye/util/log/LogUtil.java
@@ -5,25 +5,45 @@ public class LogUtil {
 	public static <T> void println(T... t) {
 		log(t);
 	}
+
+	public static <T> void logarray(T[] arr) {
+
+		for (T t : arr) {
+			print(t);
+		}
+		println();
+	}
+	public static void logarray(double[] arr) {
+		
+		for (double t : arr) {
+			print(t);
+		}
+		println();
+	}
+	public static void logarray(int[] arr) {
+		
+		for (int t : arr) {
+			print(t);
+		}
+		println();
+	}
+
 	@SafeVarargs
 	public static <T> void log(T... t) {
 
-		for (int i=0;i<t.length-1;i++) {
+		for (int i = 0; i < t.length - 1; i++) {
 			System.out.print(t[i] + " ");
 		}
-		if(t.length>0) {
-			System.out.println(t[t.length-1]);
-		}
-		else {
+		if (t.length > 0) {
+			System.out.println(t[t.length - 1]);
+		} else {
 			System.out.println();
 		}
 	}
 
-	
-	
-	public static <T> void print(T ...ts){
-		for(T t : ts) {
-			System.out.print(t+" ");
+	public static <T> void print(T... ts) {
+		for (T t : ts) {
+			System.out.print(t + " ");
 		}
 	}
 
--- src/com/xiuye/util/math/MathUtil.java
+++ src/com/xiuye/util/math/MathUtil.java
@@ -9,7 +9,90 @@ public interface MathUtil {
 		return gcd(b, r);
 	}
 
+	public static int max(int a, int b) {
+		return a >= b ? a : b;
+	}
 
+	public static double max(double a, double b) {
+		return a >= b ? a : b;
+	}
+
+	public static int max(int[] a) {
+		if (a.length <= 0)
+			throw new RuntimeException("blank array");
+		int max = a[0];
+		for (int i = 1; i < a.length; i++) {
+//			if(a[i]>max)max=a[i];
+			max = max(max, a[i]);
+		}
+		return max;
+
+	}
+
+	public static double max(double[] a) {
+		if (a.length <= 0)
+			throw new RuntimeException("blank array");
+		double max = a[0];
+		for (int i = 1; i < a.length; i++) {
+//			if(a[i]>max)max=a[i];
+			max = max(max, a[i]);
+		}
+		return max;
+
+	}
+
+	public static double average(double[] a) {
+		double sum = 0.0;
+
+		for (double i : a) {
+			sum += i;
+		}
+		return sum / a.length;
+	}
+
+	public static double average(int[] a) {
+		double sum = 0.0;
+
+		for (double i : a) {
+			sum += i;
+		}
+		return sum / a.length;
+	}
+
+	public static void reverse(int []a) {
+		for(int i=0;i<a.length/2;i++) {
+			int temp = a[i];
+			a[i] = a[a.length-i-1];
+			a[a.length-i-1] = temp;
+		}
+	}
+	public static void reverse(double []a) {
+		for(int i=0;i<a.length/2;i++) {
+			double temp = a[i];
+			a[i] = a[a.length-i-1];
+			a[a.length-i-1] = temp;
+		}
+	}
 	
-
+	public static int abs(int x) {
+		if(x<0)return -x;
+		return x;
+	}
+	public static double abs(double x) {
+		if(x<0.0)return -x;
+		return x;
+	}
+	//mark!
+	public static boolean isPrime(int x) {
+		if(x<2)return false;
+		//i <= sqrt(x) <=> i*i <= x 
+		for(int i=2;i*i<=x;i++) {
+			if(x%i==0)return false;
+		}
+		return true;
+	}
+	
+	
+	
+	
 }
